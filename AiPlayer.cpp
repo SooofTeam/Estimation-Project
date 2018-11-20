27634edc55e14f9068faa36dedd24c52df0ed2c1
Ahmed Hassan <ahmedhassanezz01@gmail.com>
2018-11-20 21:06:47 +0200

    Update AiPlayer.cpp

--- AiPlayer.cpp
+++ AiPlayer.cpp
@@ -6,7 +6,7 @@ AiPlayer::AiPlayer()
 {
 }
 
-pair<int, string>AiPlayer::Bid(vector < pair<string, int>> pc)
+pair<int, string> AiPlayer::BidCall(vector < pair<int,string>> pc)
 {
 	vector<int>comp;
 	vector<pair<int, string>>Counter;
@@ -20,22 +20,22 @@ pair<int, string>AiPlayer::Bid(vector < pair<string, int>> pc)
 	
 	for (auto i : pc) {
 
-		if (i.first == "Spades")
+		if (i.second == "Spades")
 		{
-			Shapes[0].push_back(i.second);
+			Shapes[0].push_back(i.first);
 		}
-		if (i.first == "Hearts")
+		if (i.second == "Hearts")
 		{
-			Shapes[1].push_back(i.second);
+			Shapes[1].push_back(i.first);
 		}
-		if (i.first == "Diamonds")
+		if (i.second == "Diamonds")
 		{
-			Shapes[2].push_back(i.second);
+			Shapes[2].push_back(i.first);
 		}
 
-		if (i.first == "Clubs")
+		if (i.second == "EClubs")
 		{
-			Shapes[3].push_back(i.second);
+			Shapes[3].push_back(i.first);
 		}
 	}
 	int c = 0;
@@ -80,16 +80,90 @@ pair<int, string>AiPlayer::Bid(vector < pair<string, int>> pc)
 		else if (c == 2)
 			a = "Diamonds";
 		else
-			a = "Clubs";
+			a = "EClubs";
+
 		Counter.push_back({ asc.size(),a });
 		c++;
 	}
-	sort(Counter.begin(), Counter.end());
 
+	sort(Counter.begin(), Counter.end());
 
 
 	return Counter.back();
 }
+pair<int, string> AiPlayer::MainCall(pair<int ,string> &Bidcall, vector < pair<int, string>> &pc)
+{
+	vector<int>comp;
+	int Counter=0;
+
+	vector <vector <int>> Shapes(4);
+
+	for (auto i : pc) 
+	{
+		if (i.second == Bidcall.second)
+			continue;
+
+		if (i.second == "Spades")
+		{
+			Shapes[0].push_back(i.first);
+		}
+		if (i.second== "Hearts")
+		{
+			Shapes[1].push_back(i.first);
+		}
+		if (i.second== "Diamonds")
+		{
+			Shapes[2].push_back(i.first);
+		}
+
+		if (i.second == "EClubs")
+		{
+			Shapes[3].push_back(i.first);
+		}
+	}
+
+	for (auto &i : Shapes)
+		sort(i.rbegin(), i.rend());
+
+
+	for (auto &i:Shapes)
+	{
+		
+	int counter = 0;
+
+		if (i.size() >= 3)
+		{
+
+			if (i[0] == 14 && i[1]==13)
+				counter+=2;
+			else if (i[0] == 14)
+				counter++;
+			else if (i[0] == 13)
+				counter++;
+		}
+		else if (i.size() == 2)
+		{
+			if (i[0] == 14 && i[1] == 13)
+				counter += 2;
+			else if (i[0] == 14)
+				counter++;
+		}
+		else if (i.size() == 1)
+		{
+			if (i[0] == 14)
+				counter++;
+		}
+
+		Counter+=counter;
+	}
+	pair<int, string> Call;
+	Call.first = Counter+Bidcall.first;
+	Call.second = Bidcall.second;
+	return Call;
+
+
+}
+
 
 AiPlayer::~AiPlayer()
 {
