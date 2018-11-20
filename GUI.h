8e979a54606a1e7e693a1df62bb272cd7130ce5b
Ahmed Hassan <ahmedhassanezz01@gmail.com>
2018-11-20 21:20:24 +0200

    Update GUI.h

--- GUI.h
+++ GUI.h
@@ -14,14 +14,20 @@ public:
 	Texture special;
 	Button button;
 	pair<string, int> TypeAndValue;
+	void Reset()
+	{
+		shape.setSize(Vector2f(0,0));
+		button.lowerLimit = Vector2f(0, 0);
+		button.upperLimit = Vector2f(0, 0);
+	}
 };
 class GUI
 {
 public:
 	GUI();
-	void ProgramRun(RenderWindow &window, interactiveButton Card[14], Sprite BackGround);
+	void ProgramRun(RenderWindow &window, interactiveButton Card[14], Sprite BackGround,interactiveButton RoundDone);
 	void SetTextures(Texture Spades[15], Texture Hearts[15], Texture Diamonds[15], Texture Clubs[15], Texture &Background);
-	void PlayerCardsSetup( vector < pair<string, int>> Players, interactiveButton Card[14], Texture Spades[15], Texture Hearts[15], Texture Diamonds[15], Texture Clubs[15]);
+	void PlayerCardsSetup( vector < pair<int, string>> Players, interactiveButton Card[14], Texture Spades[15], Texture Hearts[15], Texture Diamonds[15], Texture Clubs[15]);
 	void normalize(interactiveButton &ib);
 	void RectButtonAssign(interactiveButton &ib);
 	bool isButtonHovered(interactiveButton ib, RenderWindow &window);
