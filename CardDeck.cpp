675f6a20a60aa7fe36987e9ffbbd24fd7f41b632
Ahmed Hassan <ahmedhassanezz01@gmail.com>
2018-11-20 21:17:01 +0200

    Update CardDeck.cpp

--- CardDeck.cpp
+++ CardDeck.cpp
@@ -6,22 +6,22 @@ CardDeck::CardDeck()
 {
 }
 
-vector < pair<string, int>> CardDeck::Cards()
+vector < pair<int, string>> CardDeck::Cards()
 {
-	string Colors[4] = { "Spades","Hearts","Diamonds" ,"Clubs" };
-	vector < pair<string, int>> Cards;
+	string Colors[4] = { "Spades","Hearts","Diamonds" ,"EClubs" };
+	vector < pair<int, string>> Cards;
 	for (int i = 0; i < 4; i++)
 	{
 		for (int j = 2; j <= 14; j++)
 		{
-			Cards.push_back({ Colors[i], j });
+			Cards.push_back({ j, Colors[i] });
 		}
 	}
 	return Cards;
 
 }
 
-void CardDeck::Distribute(vector < vector < pair<string, int>>> &Players, vector < pair<string, int>> DeckOfCards)
+void CardDeck::Distribute(vector < vector < pair<int, string>>> &Players, vector < pair<int, string>> DeckOfCards)
 {
 	int c = 0;
 	for (int i = 0; i < 4; i++)
