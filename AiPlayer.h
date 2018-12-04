902cde6777fb941fc200b989b8622ea42e221e7c
AmirAbu-Almajd <45026376+AmirAbu-Almajd@users.noreply.github.com>
2018-12-04 09:23:39 +0200

    Update AiPlayer.h

--- AiPlayer.h
+++ AiPlayer.h
@@ -11,7 +11,7 @@ public:
 	pair<int, string> BidCall(vector < pair<int, string>> pc);
 	pair<int, string> MainCall(pair<int, string> &Bidcall, vector < pair<int, string>> &pc);
 	int FinalCall(string trump, vector<pair<int, string>> pc, int Caller, int TotalLammat, vector<pair<int, int>> &Lammat);
-	pair<int, string> CardDes(vector < pair<int, string>> &CardDeck, vector<pair<int, string>> CardsOnGround, string Trump, vector < pair<int, string>>pc, string Status, int Lammat, int FinalCall, vector<pair<int, int>> lammaty);
+	pair<int, string> CardDes(vector < pair<int, string>> &CardDeck, vector<pair<int, string>> CardsOnGround, string Trump, vector < pair<int, string>>pc, string Status, int &Lammat, int FinalCall, vector<pair<int, int>> &lammaty);
 	void PlayGround(vector<pair<int, string>> CardOnGround, string Trump, vector<int> &KolElLammat);
 	//bool  dashCall(vector < pair<int, string>> pc);
 	~AiPlayer();
